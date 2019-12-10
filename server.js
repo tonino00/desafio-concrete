80f28bc139c59e065f12e687c8ee67021f1d2934
hackgrupo71 <hackgrupo71@gmail.com>
2019-12-10 17:46:00 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router();
+app.use(logger());
+
+// Routes 
+router.get('/', (ctx, next) => {
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(3000);
